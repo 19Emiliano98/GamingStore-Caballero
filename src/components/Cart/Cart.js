e87ef856c1742957d61bebfcf9344c81091f37ef
Emiliano Caballero <emiliano.caballero98@gmail.com>
2022-07-18 21:50:26 -0300

    intentando meter la suma total de la compra

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { cartContext } from '../Context/CartContext';
 import {Link} from 'react-router-dom';
 
@@ -7,20 +7,27 @@ import "./CartStyles.css"
 const Cart = () => {
 
     const { products, qtyProducts } = useContext(cartContext)
-
-    console.log(qtyProducts)
-
+    /* const [suma, setSuma] = useState(0); */
     console.log(products)
+    
+
   return (
     <div className='container'>
         <h1>Carrito de compras</h1>
-        
         {
-            qtyProducts ===  0
-            ? <Link to='/'>No hay productos, ve a comprar aca</Link>
-            : <div>{products.map(product => <h2 key={product.id}>{product.name}</h2>)}</div>
+          qtyProducts ===  0
+          ? <Link to='/'>No hay productos, ve a comprar aca</Link>
+          : 
+          <div>
+            {products.map(product => 
+              <div key={product.id}>
+                <h2>{product.name}</h2>
+                <h3>{product.description}</h3>
+                <span>$ { product.price }</span>
+              </div>
+            )}
+          </div>
         }
-
     </div>
   )
 }
