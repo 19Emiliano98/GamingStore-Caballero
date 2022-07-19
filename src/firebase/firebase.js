7646ef9a542000f14c51b5e07d32358cd568f9ed
Emiliano Caballero <emiliano.caballero98@gmail.com>
2022-07-19 20:46:34 -0300

    se le instalo e implemento al app el firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNbTBY88Sx3ulEVdBw5M9NEqPvsgJqjpw",
+  authDomain: "gaming-store-c85f1.firebaseapp.com",
+  projectId: "gaming-store-c85f1",
+  storageBucket: "gaming-store-c85f1.appspot.com",
+  messagingSenderId: "413190582842",
+  appId: "1:413190582842:web:dc68d487d57f68e499bb03"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
